620c4a72f16db3c5afed0eb5d64cb5de7f390bc3
mariocelic <mariocelic@gmail.com>
2019-08-18 08:54:01 +0200

    ispravljen pristup bazi

--- baza.php
+++ baza.php
@@ -1,21 +1,12 @@
 <?php
 
-switch ($_SERVER["REMOTE_ADDR"]) {
-    case '127.0.0.1': //moj lokalni razvoj
-        $server="localhost";
-        $baza="hotel";
-        $korisnik="mario";
-        $lozinka="mario";
-        break;
-    
-    case '31.147.185.242': //edunova shared hosting
-        $server="localhost";
-        $baza="tanatos_edunovapp19";
-        $korisnik="tanatos_edunova";
-        $lozinka="edunova";
-        break;
-}
+// za provjeru greški
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 
-$veza = new PDO("mysql:dbname=" . $baza . ";host=" . $server,
-$korisnik,$lozinka,[PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"]);
+$veza = new PDO("mysql:dbname=tanatos_edunovapp19;host=localhost",
+"tanatos_edunova","Tanatos90835",[PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"]);
+
+
