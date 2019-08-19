ce6684808b3e5c70c341695397c4ea82d068dcf4
mariocelic <mariocelic@gmail.com>
2019-08-19 16:36:58 +0200

    Dodan i drugi korisnik

--- autoriziraj.php
+++ autoriziraj.php
@@ -1,19 +1,29 @@
 <?php
 include_once "konfiguracija.php";
 
+$korisnik1 = "edunova@edunova.hr";
+$korisnik2 = "mariocelic@gmail.com";
+$lozinka1 = "edunova";
+$lozinka2 = "mario";
+
 if(!$_POST || !isset($_POST["email"]) || !isset($_POST["lozinka"])){
     exit;
 }
 
 //uvdje će doći autorizacija na bazu
-if($_POST["email"]==="edunova@edunova.hr" && $_POST["lozinka"]==="edunova"){
+if (($_POST["email"]==$korisnik1) && ($_POST["lozinka"]==$lozinka1)){
 
     //autoriziran sam
     $_SESSION["autoriziran"]="edunova";
     header("location: privatno.php");
 
-}else{
+} else if(($_POST["email"]==$korisnik2) && ($_POST["lozinka"]==$lozinka2)){
 
+    //autoriziran sam
+    $_SESSION["autoriziran"]="mario";
+    header("location: privatno.php");
+
+} else {
     //nisam autoriziran
     header("location: index.php?g=1");
 
